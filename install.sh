88862a501dddcc9096d95604fab8db33a01610ac
Chris Pergrossi <c.pergrossi@chr1s.co>
2016-02-10 00:39:54 -0500

    updated bashrc and added an install file

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo apt-get install golang nodejs build-essential cmake make gnuradio hackrf rtl-sdr python-pip
+
+sudo pip install thefuck scipy numpy
+
+mkdir -p ~/dev/go/src ~/dev/go/pkg ~/dev/go/bin ~/school ~/dev/js ~/dev/python
+
+echo "Install Virtual Box/VMWare"
+echo "Install Google-Chrome"
+echo "Install Steam"
