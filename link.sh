6e7c03b93db5f3986becef24e379bc7158d1a23e
Chris Pergrossi <c.pergrossi@ufl.edu>
2016-04-12 00:26:43 -0400

    added vim colorscheme and updated installation

--- link.sh
+++ link.sh
@@ -6,12 +6,22 @@
 # Author Chris Pergrossi
 # GPG License
 
+# loop through all dot files
 for F in \.*; do
 	[[ "$F" == "."  ]] && continue
 	[[ "$F" == ".."  ]] && continue
 	[[ "$F" =~ \.git  ]] && continue
 
+    # if file exists, store as backup
 	[ -e "$HOME/$F" ] && mv "$HOME/$F" "$HOME/$F.bak"
+    
+    # symlink the dotfile so any changes can be commited
+    ln -s "`pwd`/$F" "$HOME/$F"
 
-	ln -s "`pwd`/$F" "$HOME/$F"
 done
+
+# install awesome-vim
+./awesome-vim.sh
+
+# copy our colorscheme choice to VIM folder
+ln -s "$(pwd)/my_configs.vm" "$HOME/.vim_runtime/"
